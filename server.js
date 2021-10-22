c38cb0daada1d7a3c02e392f1a8e843edaab8a87
Brendan Beck <brendan.beck62@gmail.com>
2021-10-22 12:03:23 -0700

    basic node + express server running

---
+++ server.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const express = require('express');
+const url = require('url');
+const favicon = require('express-favicon');
+const fs = require('fs');
+
+// Constants
+// TODO .env this badboy
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+// App test
+const app = express();
+
+app.use(express.static(__dirname + '/public/static/'));
+app.use(favicon(__dirname + '/public/static/img/favicon.ico'));
+app.set('views', __dirname + '/public/views')
+
+app.set('view engine', 'ejs');
+
+
+app.get(/^(?!\/api\/)/, (req, res) => {
+  let purl = url.parse(req.url, true);
+  let pathname = 'pages' + purl.pathname;
+
+  if ((pathname)[pathname.length - 1] === '/') {
+      pathname += 'index';
+  }
+
+  // add the path + extension, so that the fs.access can find it
+  var pathPrepend = "./public/views/";
+  var extenstion = ".ejs"
+  var fullpath = pathPrepend.concat(pathname).concat(extenstion);
+
+  // check if the file exists in the pages directory
+  fs.access(fullpath, fs.F_OK, (err) => {
+      // if it does not exist, display the 404 page
+      if (err) {
+          res.render("pages/404", purl.query);
+          return
+      }
+
+      // otherwise display the page
+      res.render(pathname, purl.query);
+    })
+
+});
+
+app.listen(PORT, () => {
+  console.log(`app listening at http://localhost:${PORT}`)
+})
